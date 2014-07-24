f43e31f58292b235bd64a9779997f2445b8fc1f8
ppp <ppp@gmail.com>
2014-07-24 14:35:54 +0530

    commit to master

--- src/com/crest/DemoMain.java
+++ src/com/crest/DemoMain.java
@@ -13,4 +13,10 @@ public static void main(String str){
 	System.out.println("We are in Master updated again");
 	System.out.println("We are in Master updated on r1");
 }
+
+public void method1(){
+	
+	System.out.println("we are in method1 for see the diff");
+}
+
 }
